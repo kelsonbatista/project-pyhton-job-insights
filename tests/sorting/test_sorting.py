c2ff08152e99deaa06320c66f7d3096f6be14534
kelsonbatista <kelsonbatista@gmail.com>
2022-09-13 19:12:00 -0300

    feat: added test sorting function

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,54 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+# from src.jobs import read
+
+jobs = [{
+    'title': 'Full stack end developer',
+    'min_salary': '4000',
+    'max_salary': '8000'
+}, {
+    'title': 'Back end developer',
+    'min_salary': '',
+    'max_salary': '3000'
+}, {
+    'title': 'Front end developer',
+    'min_salary': '1000',
+    'max_salary': ''
+}]
+
+sort_max_salary = [{
+    'title': 'Full stack end developer',
+    'min_salary': '4000',
+    'max_salary': '8000'
+}, {
+    'title': 'Back end developer',
+    'min_salary': '',
+    'max_salary': '3000'
+}, {
+    'title': 'Front end developer',
+    'min_salary': '1000',
+    'max_salary': ''
+}]
+
+sort_min_salary = [{
+    'title': 'Front end developer',
+    'min_salary': '1000',
+    'max_salary': ''
+}, {
+    'title': 'Full stack end developer',
+    'min_salary': '4000',
+    'max_salary': '8000'
+}, {
+    'title': 'Back end developer',
+    'min_salary': '',
+    'max_salary': '3000'
+}]
 
 
 def test_sort_by_criteria():
-    pass
+    # path = 'tests/mocks/jobs_with_salaries.csv'
+    # jobs = read(path)
+
+    sort_by(jobs, "max_salary")
+    assert jobs == sort_max_salary
+    sort_by(jobs, "min_salary")
+    assert jobs == sort_min_salary
