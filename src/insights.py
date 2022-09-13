ec88416f29f7012710e6238e9c7a70919b0f6297
kelsonbatista <kelsonbatista@gmail.com>
2022-09-12 22:41:08 -0300

    feat: added get_min_salary function

--- src/insights.py
+++ src/insights.py
@@ -133,7 +133,13 @@ def get_min_salary(path):
     int
         The minimum salary paid out of all job opportunities
     """
-    pass
+    jobs = read(path)
+    min_salary = [
+        int(job["min_salary"])
+        for job in jobs
+        if job["min_salary"].isdigit()
+    ]
+    return min(min_salary)
 
 
 def matches_salary_range(job, salary):
