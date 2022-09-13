5a11f8c5a45870b222bdf0bae35f6925946a24d8
kelsonbatista <kelsonbatista@gmail.com>
2022-09-13 16:31:53 -0300

    fix: changed assert return

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,8 +1,7 @@
 from src.counter import count_ocurrences
-from tests.counter.mocks import count_word_ocurrences
 
 
 def test_counter():
-    count = count_ocurrences("src/jobs.csv", "Javascript")
-    result = count_word_ocurrences("src/jobs.csv", "Javascript")
-    assert result != count
+    path = 'src/jobs.csv'
+    count = count_ocurrences(path, "javascript")
+    assert count == 122
