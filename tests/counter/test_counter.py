2243f06c7a0b429102d88219a2ef17475e305e6c
kelsonbatista <kelsonbatista@gmail.com>
2022-09-13 10:25:42 -0300

    feat: added test_counter function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+from tests.counter.mocks import count_word_ocurrences
 
 
 def test_counter():
-    pass
+    count = count_ocurrences("src/jobs.csv", "Javascript")
+    result = count_word_ocurrences("src/jobs.csv", "Javascript")
+    assert result != count
