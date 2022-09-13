46170db6c19744ce504c415f56955439cb7b887c
kelsonbatista <kelsonbatista@gmail.com>
2022-09-13 13:06:12 -0300

    feat: added test brazilian jobs function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = 'tests/mocks/brazilians_jobs.csv'
+    result = {'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'}
+    assert result in read_brazilian_file(path)
